7036fc3c93e58bf82ab46d006048b18b186740d7
aurelien briard <aurelienbriard@gmail.com>
2020-10-09 17:00:30 +0200

    TODOLIST
    Commit fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,37 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2kyXbjBeXDYB7sgFjQ9-9IbQVC1qHPpE",
+  authDomain: "todolist-d6792.firebaseapp.com",
+  databaseURL: "https://todolist-d6792.firebaseio.com",
+  projectId: "todolist-d6792",
+  storageBucket: "todolist-d6792.appspot.com",
+  messagingSenderId: "57265143282",
+  appId: "1:57265143282:web:a49c033eae6b1340c43f6b",
+  measurementId: "G-PX9LLZ2R0W"
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+     BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule,
+     AngularFireModule.initializeApp(firebaseConfig),
+     AngularFireDatabaseModule,
+     AngularFireAuthModule,
+     AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// connexion avec firebase
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,67 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // ----------------------------  mes variables ---------------------------------
+  currentDate: string;
+
+  myTask = '';// je déclare une tache vide
+  tasks = []; // le tableau de taches
+
+  addTask: boolean; // un bolléen qui prend vrai ou faux si la tâche est affichée ou non
+
+  // le constructeur de la page
+  constructor(public afDB: AngularFireDatabase) {
+    const date = new Date(); // je crée une variable date
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.getTasks();
+  }
+
+  // ------------------------------------ mes fonctions ------------------------------------
+
+  // pour ajouter une tache
+  addTaskToFireBase() {
+    this.afDB.list('Tasks/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+    this.showForm();
+  }
+
+  // pour montrer une tache
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+
+  // recuperer les taches de la base de donnée
+  getTasks() {
+  this.afDB.list('Tasks/')
+    .snapshotChanges(['child_added', 'child_removed'])
+    .subscribe(actions => {
+    this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          hour: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+
+  // gestion des checked
+  changeCheckState(ev: any) {
+    console.log('checked: ' + ev.checked);
+    this.afDB.object('Tasks/' + ev.key + '/checked/').set(ev.checked);
+  }
+
+  // supprimer une tache
+  deleteTask(task: any) {
+  this.afDB.list('Tasks/').remove(task.key);
+  }
+
 
 }
